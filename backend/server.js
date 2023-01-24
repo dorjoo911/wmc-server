d248cc130c73a786495937f68a0d232ffdd4463f
dorjoo911 <73855281+dorjoo911@users.noreply.github.com>
2023-01-24 14:48:43 -0500

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Response = require("./responseObj");
+
+const authRouter = require("./authRouter");
+const userRouter = require("./user/userRouter");
+const postRouter = require("./post/postRouter");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", userRouter);
+app.use("/login", authRouter);
+app.use("/posts", postRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).json(new Response(true, err.message, null));
+});
+
+mongoose
+  .connect("mongodb+srv://wmc:wmc20@wmccluster1.5v650it.mongodb.net/test")
+  .then(() => {
+    app.listen(4000, () => console.log("listening ... 4000"));
+  });
